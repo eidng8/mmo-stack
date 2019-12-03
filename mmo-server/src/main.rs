37d5c27de53f5c7f4ff494fe996943b3da7209b1
eidng8 <cheung.jackey@gmail.com>
2019-12-03 16:25:14 +0800

    Switches to `tokio`, and do a simple TCP echo server.

--- mmo-server/src/main.rs
+++ mmo-server/src/main.rs
@@ -1,17 +1,45 @@
 /*
  * GPLv3 ( https://opensource.org/licenses/GPL-3.0 )
  */
+use tokio::net::TcpListener;
+use tokio::prelude::*;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:9000").await?;
+    println!("Server socket ready.");
 
-use crate::listeners::udp;
+    loop {
+        let (mut stream, addr) = listener.accept().await?;
 
-//mod binary;
-//mod commands;
-//mod game;
-mod listeners;
+        tokio::spawn(async move {
+            let mut got: String;
+            let mut buf = [0; 1024];
 
-fn main() {
-    if let Ok(thread) = udp::listen("127.0.0.1:9000".parse().unwrap(), 1) {
-        thread.join().unwrap();
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                match stream.read(&mut buf).await {
+                    Ok(n) if n == 0 => return, // socket closed
+                    Ok(n) => {
+                        got = String::from_utf8_lossy(&buf[..n]).into_owned();
+                        println!("Got data from client {} > {}", addr, got);
+                        n
+                    }
+                    Err(e) => {
+                        println!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                let msg = format!("{} [{}]", got, addr);
+                match stream.write_all(msg.as_bytes()).await {
+                    Ok(_) => {
+                        println!("Sent back to client {}  < {}", addr, msg)
+                    }
+                    Err(e) => println!("Failed to write to socket; err = {:?}", e),
+                }
+            }
+        });
     }
 }
