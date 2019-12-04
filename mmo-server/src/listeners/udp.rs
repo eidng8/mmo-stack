180fd2f01faa0811c840ac026e3cf3e585b9faeb
eidng8 <jackey.cheung@gmail.com>
2019-12-04 19:54:39 +0800

    move buffer out of loop and enlarge it

--- mmo-server/src/listeners/udp.rs
+++ mmo-server/src/listeners/udp.rs
@@ -3,13 +3,13 @@
  */
 
 use std::net::SocketAddr;
-use tokio::net::UdpSocket;
 use tokio::net::udp::SendHalf;
+use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
 
 #[derive(Debug)]
 struct Content {
-    msg: String
+    msg: String,
 }
 
 #[derive(Debug)]
@@ -26,6 +26,7 @@ struct RemoteContent {
 /// ## Arguments
 /// * `addr` - IP address and port to listen on
 pub async fn listen(addr: &str) {
+    let mut buf = [0; 65535];
     let socket = UdpSocket::bind(addr).await.unwrap();
     println!("UDP socket ready. [{}]", addr);
     let (mut r, w) = socket.split();
@@ -34,7 +35,6 @@ pub async fn listen(addr: &str) {
     tokio::spawn(send(rx, w));
 
     loop {
-        let mut buf = [0; 1024];
         match r.recv_from(&mut buf).await {
             Ok((cb, _)) if cb == 0 => return, // socket closed
             Ok((cb, addr)) => process(&buf, cb, addr, &mut tx).await,
@@ -45,7 +45,6 @@ pub async fn listen(addr: &str) {
         };
     }
 }
-
 
 /// Process *one* received message. Pushes the message to the `mpsc::channel`.
 ///
@@ -59,15 +58,16 @@ async fn process(buf: &[u8], cb: usize, addr: SocketAddr, tx: &mut mpsc::Sender<
     let msg = format!("Got ya [{}]! Ya said {}", addr, got);
     let content = RemoteContent {
         addr,
-        content: Content { msg: String::from(msg) },
+        content: Content {
+            msg: String::from(msg),
+        },
     };
     match tx.send(content).await {
         Ok(_) => (),
-        Err(e) => println!("Failed to push to message channel: {:?}", e)
+        Err(e) => println!("Failed to push to message channel: {:?}", e),
     }
     println!("Got data from UDP client {} > {}", addr, got);
 }
-
 
 /// Process message on the `mpsc::channel`, sending them to remote peer.
 ///
@@ -87,15 +87,8 @@ async fn send(mut rx: mpsc::Receiver<RemoteContent>, mut stream: SendHalf) {
             let addr = remote.addr;
             let msg = remote.content.msg;
             match stream.send_to(msg.as_bytes(), &addr).await {
-                Ok(_) => println!(
-                    "Sent back to UDP client {}  < {}",
-                    addr,
-                    msg
-                ),
-                Err(e) => println!(
-                    "Failed to write to UDP socket; err = {:?}",
-                    e
-                ),
+                Ok(_) => println!("Sent back to UDP client {}  < {}", addr, msg),
+                Err(e) => println!("Failed to write to UDP socket; err = {:?}", e),
             }
         }
     }
